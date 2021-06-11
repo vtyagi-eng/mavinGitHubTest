d86d3591a8394d3437f69fa6a977fee33cbc3de3
vtyagi-eng <85718252+vtyagi-eng@users.noreply.github.com>
2021-06-11 07:52:24 +0530

    Update fbLogin.java

--- src/test/java/GitHubtest1/GitHubtest1/fbLogin.java
+++ src/test/java/GitHubtest1/GitHubtest1/fbLogin.java
@@ -13,6 +13,7 @@ public class fbLogin
 		driver= new ChromeDriver();
 		driver.get("https://en-gb.facebook.com/login/");
 		driver.manage().window().maximize();
+		//maximizing the screeen
 	}
 	
 	public void login()
