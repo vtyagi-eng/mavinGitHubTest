3a9338e660bc783a550ac7e401959a855f1ddead
Vijay r. Tyagi <Vijay r. Tyagi@192.168.1.9>
2021-06-11 07:45:10 +0530

    I am commiting my first check in.

---
+++ src/main/java/GitHubtest1/GitHubtest1/App.java
@@ -0,0 +1,13 @@
+package GitHubtest1.GitHubtest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/GitHubtest1/GitHubtest1/fbLogin.java
@@ -0,0 +1,33 @@
+package GitHubtest1.GitHubtest1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class fbLogin 
+{
+	public WebDriver driver;
+	public void Setup()
+	{
+		System.setProperty("webdriver.chrome.driver","D:\\Software\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("https://en-gb.facebook.com/login/");
+		driver.manage().window().maximize();
+	}
+	
+	public void login()
+	{
+		driver.findElement(By.id("email")).sendKeys("tyagivivek21@yahoo.co.in");
+		driver.findElement(By.name("pass")).sendKeys("12345678");
+		driver.findElement(By.name("login")).click();
+	}
+
+	public static void main(String[] args) {
+
+		fbLogin l= new fbLogin();
+		l.Setup();
+		l.login();
+	}
+
+
+}
